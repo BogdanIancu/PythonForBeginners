7c21505072d4d39f49d06644f70384ec81b57d1b
Bogdan Iancu <bogdan.iancu@ymail.com>
2023-11-28 18:29:08 +0200

    Update main.py

--- Course12/main.py
+++ Course12/main.py
@@ -1,4 +1,20 @@
 import datetime
+import functools
+
+
+class MyIterableType:
+    def __init__(self):
+        self.value = -2
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value < 10:
+            self.value += 2
+            return self.value
+        else:
+            raise StopIteration
 
 
 def main():
@@ -14,11 +30,19 @@ def main():
     for c in my_string:
         print(c)
 
+    # this is a custom iterator
+    my_iterator = MyIterableType()
+    for i in my_iterator:
+        print(i)
+
 
 # Decorator
 def logger(function):
-    print(f"Function {function.__name__} called at {datetime.datetime.now()}")
-    return function
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        print(f"Function {function.__name__} called at {datetime.datetime.now()}")
+        return function(*args, **kwargs)
+    return wrapper
 
 
 @logger
@@ -26,6 +50,13 @@ def say_hello():
     print("Hello world!")
 
 
+@logger
+def addition(x, y):
+    return x + y
+
+
 if __name__ == '__main__':
     main()
     say_hello()
+    print(addition(3, 7))
+    print(say_hello.__name__)
